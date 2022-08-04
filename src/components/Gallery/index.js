6d066f2f5e4eee2bb8ae6c9dae68946fc4a60b79
escowin <edwin@escowinart.com>
2022-08-04 01:44:42 -0500

    20.3.4 | Gallery js:27 note

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import photo from "../../assets/small/commercial/0.jpg"
+
+function Gallery(props) {
+    // using {object} data structure, every gallery page will have name & description. 
+    const currentCategory = {
+        name: "commercial",
+        descrption: "Photos of grocery stores, food trucks, and other commercial projects",
+    };
+
+    return (
+        <section>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{capitalizeFirstLetter(currentCategory.descrption)}</p>
+            <div>
+                <img 
+                 src={photo}
+                 alt="commercial example"
+                 className="img-thumbnail mx-1"
+                />
+            </div>
+        </section>
+    );
+}
+
+// ** paused 20.3.4
+export default Gallery;
