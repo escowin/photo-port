7f50e7048163ecb77d3fd2e7b942b39b176671b0
escowin <edwin@escowinart.com>
2022-08-04 00:07:14 -0500

    20.1.6 | + <Nav>, <About> | + notes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function Nav() {
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
+
     const categories = [
         {
             name: "commercial",
@@ -37,9 +41,17 @@ function Nav() {
                     <li>
                         <span>Contact</span>
                     </li>
+                    {/* [categories {objects}] are mapped over as "category" argument being passed thru map(). all 4 {name: ...} display as seperate <li> elements */}
                     {categories.map((category) => (
-                        <li className="mx-1" key={category.name}>
-                            <span>{category.name}</span>
+                        // Whenever anything is mapped in JSX, the outermost element must have a uniquely-set key attribute. React keeps track of items in the virtual DOM
+                        <li
+                         className="mx-1" 
+                         key={category.name}
+                        >
+                            {/* onClick expects a callback function declaration. It's important to wrap it in a function declaration rather than just calling categorySelected(category.name), which would cause the function to get called whenever the component renders as well. */}
+                            <span onClick={() => categorySelected(category.name)} >
+                                {category.name}
+                            </span>
                         </li>
                     ))}
                 </ul>
